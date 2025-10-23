52d7875729fa5622cee5851ece9d13ecd7e9395e
Andrey121998 <oblom007@yandex.ru>
2025-10-24 01:42:10 +0300

    закончил урок 1

--- lessonOne.php
+++ lessonOne.php
@@ -38,7 +38,41 @@ class User {
 }
 $user = new User(1, 'Иван', 'ivan');
 echo $user->name. '<br>';
-echo $user->name = 'Андрей'. '<br>';
+echo "место для ошибки". '<br>';//$user->name = 'Андрей'. '<br>';
 '<br>';
 // задача 4
+enum OrderStatus: string {
+    case Pending = 'pending';
+    case Shipped = 'shipped';
+    case Delivered = 'delivered';
+}
+
+function getDeliveryMessage(OrderStatus $status): string {
+     match ($status) {
+        OrderStatus::Pending => $return ='Заказ в ожидании',
+        OrderStatus::Shipped => $return ='Заказ отправлен',
+        OrderStatus::Delivered => $return ='Заказ доставлен',
+    };
+    return $return;
+}
+
+echo getDeliveryMessage(OrderStatus::Pending). '<br>';  
+echo getDeliveryMessage(OrderStatus::Shipped). '<br>';  
+echo getDeliveryMessage(OrderStatus::Delivered). '<br>';
+'<br>';
 // задача 5
+function getUserEmail(?object $user): string {
+    return $user?->profile?->email ?? 'Email не найден';
+}
+$user1 = (object)[
+    'profile' => (object)[
+        'email' => 'email'
+    ]
+];
+echo getUserEmail($user1). '<br>'; 
+
+$user2 = (object)[
+    'profile' => null
+];
+echo getUserEmail($user2). '<br>';
+echo "________________________________Блок 2________________________________" . '<br>';
