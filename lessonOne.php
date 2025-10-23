a9fcd81ba0664261c4e8ddf23443de592f3578c1
Andrey121998 <oblom007@yandex.ru>
2025-10-24 00:59:04 +0300

    делаем блок1

--- lessonOne.php
+++ lessonOne.php
@@ -1,1 +1,44 @@
-<?php echo "hi";
+<?php 
+declare(strict_types=1);
+echo "________________________________Блок 1________________________________" . '<br>';
+// задача 1.
+function getStatusMessage(string $status): string {
+    match($status){
+        'success' => $return = 'Успех',
+        'error'   => $return ='Ошибка',
+        'pending' => $return ='Ожидание',
+        default   => $return ='Неизвестно'
+    };
+    return $return;
+}
+$testArray = ['success','error','pending', '' ];
+foreach($testArray  as $row){
+    echo getStatusMessage($row). '<br>';
+}
+// задача 2
+'<br>';
+function getCalculatePrice(float $basePrice, float $discount , float $tax  ):float{
+    $disPrice = $basePrice - (($basePrice / 100)*$discount) ;
+    $calculate = $disPrice + (($disPrice / 100) * $tax);
+    return $calculate; 
+}
+echo getCalculatePrice(basePrice: 1000, discount: 10, tax: 5) .'<br>';
+echo getCalculatePrice(tax: 5, discount: 10, basePrice: 2000) . '<br>';
+// задача 3
+class User {
+  public readonly int $id;
+  public readonly string $name;
+  public readonly string $email;
+  public function __construct(int $id, string $name, string $email)
+  {
+    $this->id = $id;
+    $this->name = $name;
+    $this->email = $email;
+  }  
+}
+$user = new User(1, 'Иван', 'ivan');
+echo $user->name. '<br>';
+echo $user->name = 'Андрей'. '<br>';
+'<br>';
+// задача 4
+// задача 5
