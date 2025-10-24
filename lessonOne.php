a03f0c8c1b212296e31670009f590979d4c57ab3
Andrey121998 <oblom007@yandex.ru>
2025-10-24 20:19:06 +0300

    конец основ

--- lessonOne.php
+++ lessonOne.php
@@ -122,3 +122,177 @@ function formatValue(int|float|string $value): string{
 echo formatValue('tax'). '<br>';
 echo formatValue(50). '<br>';
 echo "________________________________Урок_3________________________________" . '<br>';
+// задача 1
+function filterEvenNumbers(array $numbers): array
+{
+    return array_filter($numbers, function($number) {
+        return $number % 2 === 0;
+    });
+}
+
+print_r(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+//задача 2
+echo "/задача 2/ ";
+function squareNumbers(array $numbers): array
+{
+    return array_map(function($number) {
+        return $number * $number;
+    }, $numbers);
+}
+print_r(squareNumbers([1, 2, 3, 4]));
+//задача 3
+echo "/задача 3/ ";
+function getUserEmails(array $users): array
+{
+    return array_map(function($user) {
+        return $user['email'];
+    }, $users);
+}
+
+$users = [
+    ['id' => 1, 'name' => 'Alice', 'email' => 'alice@example.com'],
+    ['id' => 2, 'name' => 'Bob', 'email' => 'bob@example.com'],
+];
+
+print_r(getUserEmails($users));
+echo '<br>' . "________________________________Урок_4________________________________" . '<br>';
+// задача 1
+function checkNumber(int $int):string{
+    if ($int > 0){
+        return "положительное";
+    }else if ($int < 0){
+        return "отрицательное";
+    }else{
+        return "ноль";
+    }
+}
+$numArr=[1,-3,0];
+foreach($numArr as $row){
+    echo checkNumber($row). '<br>' ;
+}
+// задача 2
+function getAgeCategory(int $age):string{
+    return match(true) {
+        $age >= 0 && $age <= 12 => "Ребенок",
+        $age >= 13 && $age <= 17 => "Подросток",
+        $age >= 18 && $age <= 64 => "Взрослый",
+        $age >= 65 => "Пожилой",
+        default => "некорректный возраст"
+    };
+}
+
+echo getAgeCategory(8) . '<br>';    
+echo getAgeCategory(15) . '<br>';   
+echo getAgeCategory(25) . '<br>';   
+echo getAgeCategory(70) . '<br>';   
+// задача 3
+function printNumbers(int $num):void{
+    for ( $i = 1; $i <= $num; $i++ ){
+        echo $i . '<br>';
+    }
+}
+$numTest = 6;
+printNumbers($numTest);
+// задача 4
+function factorial(int $num): void{
+    $facnum = $num;
+    $i = 1;
+    // for ( $i = 1; $i <= $num; $i++ ){
+        
+    //     $facnum = $facnum * $i;
+    // }
+    while ($i <= $num){
+        $facnum = $facnum * $i;
+        $i++;
+    }
+    echo $facnum . '<br>';
+}
+factorial(5);
+// задача 5
+function printArrayItems(array $arr):void{
+    foreach($arr as $row){
+        echo $row . '<br>';
+    }
+}
+printArrayItems(["apple", "banana", "cherry"]);
+// задача 6
+function printEvenNumbers(int $num):void {
+    $i = 1;
+    while($i <= $num){
+        if($i % 2 !==0){
+            $i++;
+            continue;
+        }
+        echo $i . '<br>'; 
+        $i++;
+    }
+}
+printEvenNumbers(10);
+echo '<br>' . "________________________________Урок_5________________________________" . '<br>';
+// задача 1
+function greetUser(string $name, string $lang = 'ru'):void{
+    if($lang !=='ru'){
+        echo 'hello, ' . $name . '!'. '<br>';
+    }else{
+         echo 'привет, ' . $name . '!'. '<br>';
+    }
+}
+greetUser('Jhon','er');
+greetUser('Андрей');
+// задача 2
+function calculateDiscount(float $price, float $discount = 0.1): void{
+    if($discount<1){
+    $total= $price - ($price * $discount);
+    echo round($total,2). '<br>';
+    }else{ $total= $price -(($price * $discount)/100);
+    echo round($total,2). '<br>';
+    }
+}
+calculateDiscount(100);
+calculateDiscount(100, 20);
+// задача 3
+function orderPizza(string $size = 'medium', string $crust ='thin', array $toppings = ["cheese"]):void{
+    echo 'вы заказали ' . $size . ' пиццу на ' . $crust . ' тесте с добавлением ';
+    foreach($toppings as $row){
+        echo $row . ' ';
+    }
+    echo '<br>'; 
+}
+orderPizza();
+orderPizza('extra large', 'extra thin', ['two', 'number', 'nines', 'and', 'big', 'soda']);
+// задача 4
+function formatText(string $text, bool $uppercase = false): string {
+    if ($uppercase) {
+        return strtoupper($text);
+    }
+    return $text;
+}
+
+echo formatText("hello").'<br>'; 
+echo formatText("hello", true).'<br>';  
+echo formatText("hello", false).'<br>'; 
+// задача 5
+function generatePassword(int $length = 8, bool $includeNumbers = true, bool $includeSpecialChars = false): string {
+    $letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $numbers = '0123456789';
+    $specialChars = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+    
+    $characters = $letters;
+    if ($includeNumbers) {
+        $characters .= $numbers;
+    }
+    if ($includeSpecialChars) {
+        $characters .= $specialChars;
+    }
+    
+    $password = '';
+    $charCount = strlen($characters);
+    
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $characters[random_int(0, $charCount - 1)];
+    }
+    
+    return $password;
+}
+echo generatePassword() .'<br>';
+echo generatePassword(length: 12, includeSpecialChars: true) .'<br>';
